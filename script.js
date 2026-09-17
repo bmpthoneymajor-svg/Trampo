d288dc5408c2a84e0d0bfb5fd607f46a0de4626f
BrennoTreggi <330190548+bmpthoneymajor-svg@users.noreply.github.com>
2026-09-16 23:28:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://trampo.up.railway.app';
+const API_URL = 'https://trampo.up.railway.app'.replace(/\/$/, '');
 // CALCULAR TOTAL
 function calcularTotal() {
     let total = 0;
